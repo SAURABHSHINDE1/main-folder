9a8ef46d4a56ee62eabc4bdb36c521c4f449621a
SAURABHSHINDE1 <saurabhshinde779845@gmail.com>
2024-09-12 19:44:24 +0530

    updated on 12 sep 2024

--- to do application/script.js
+++ to do application/script.js
@@ -2,16 +2,42 @@ function createtask(){
 
     var task = document.createElement("div");
     task.className="tasks";
-     var mainrasks=document.getElementById("maintask");
-     mainrasks.appendChild(task);
+     var maintasks=document.getElementById("maintask");
+     maintasks.appendChild(task);
 
     var data = document.createElement("h1");
     task.appendChild(data);
     var inputs = document.getElementById("searchinput");
    data.textContent= inputs.value;
 
-    
-    
-
-
+   var checkbox=document.createElement("input");
+   checkbox.type="checkbox";
+   task.appendChild(checkbox);
 }
+
+
+
+function checktask(){
+  
+    var heading=document.getElementById("heading");
+    var data=document.querySelector("h1");
+   
+var checkbox1=document.getElementById("check");
+if(checkbox1.checked)
+{
+    heading.style.textDecoration="line-through";
+    data.style.textDecoration="line-through";
+    
+}
+else
+{
+    heading.style.textDecoration="none";
+    data.style.textDecoration="none ";
+}
+}
+
+
+    
+    
+
+
